8ab2dfacefdce39e31ec7a0249d4eb0c33fd271b
L-E-Imel <93445437+L-E-Imel@users.noreply.github.com>
2022-02-04 19:45:22 -0500

    fix buttons, style projects

--- script.js
+++ script.js
@@ -1,46 +1,71 @@
-/* let flipElement = document.querySelectorAll('flip');
-function showDetail() {
-    let diamondImg = document.querySelectorAll('flip img');
-    let skillName = document.querySelectorAll('flip h3');
-    let skillDescr = document.querySelectorAll('flip p');
-    diamondImg.display = 'none';
-    skillName.display = 'none';
-    skillDescr.display = 'display';
-    flipElement.style.backgroundColor = 'green';
+function showDetailOne() {
+    document.getElementById('one').firstChild.style.opacity = '0%';
+    document.getElementById('one').lastChild.style.opacity = '100%';
 }
-flipElement.onclick = showDetail(); */
-
-function getTarget(evnt) {
-    return evnt.target;
-};
-function showDetail(evnt) {
-    target = getTarget(evnt);
-    if (target === document.querySelector('ul.h3') || target === document.querySelector('ul.img')) {
-        target = target.parentNode;
-    };
-    target.querySelector('h3').style.display = 'none';
-    target.querySelector('img').style.display = 'none';
-    target.querySelector('p').style.display = 'contents'; 
-};
-
-function removeDetail(evnt) {
-    target = getTarget(evnt);
-    if (target === document.querySelector('ul.h3') || target === document.querySelector('ul.img')) {
-        target = target.parentNode;
-        console.log(target);
-    };
-    target.querySelector('h3').style.display = 'block';
-    target.querySelector('img').style.display = 'inline';
-    target.querySelector('p').style.display = 'none';
+let slideElementOne = document.getElementById('one');
+slideElementOne.addEventListener('pointerdown', showDetailOne, false);
+function hideDetailOne() {
+    document.getElementById('one').firstChild.style.opacity = '100%';
+    document.getElementById('one').lastChild.style.opacity = '0%';
 }
+slideElementOne.addEventListener('pointerup', hideDetailOne, false);
 
-let flipElement = document.getElementById('flip');
-flipElement.addEventListener('pointerdown', function(evnt) {
-    showDetail(evnt);
-}, false);
+function showDetailTwo() {
+    document.getElementById('two').firstChild.style.opacity = '0%';
+    document.getElementById('two').lastChild.style.opacity = '100%';
+}
+let slideElementTwo = document.getElementById('two');
+slideElementTwo.addEventListener('pointerdown', showDetailTwo, false);
+function hideDetailTwo() {
+    document.getElementById('two').firstChild.style.opacity = '100%';
+    document.getElementById('two').lastChild.style.opacity = '0%';
+}
+slideElementTwo.addEventListener('pointerup', hideDetailTwo, false);
 
-//evnt.preventDefault();
+function showDetailThree() {
+    document.getElementById('three').firstChild.style.opacity = '0%';
+    document.getElementById('three').lastChild.style.opacity = '100%';
+}
+let slideElementThree = document.getElementById('three');
+slideElementThree.addEventListener('pointerdown', showDetailThree, false);
+function hideDetailThree() {
+    document.getElementById('three').firstChild.style.opacity = '100%';
+    document.getElementById('three').lastChild.style.opacity = '0%';
+}
+slideElementThree.addEventListener('pointerup', hideDetailThree, false);
 
-flipElement.addEventListener('pointerup', function(evnt) {
-    removeDetail(evnt);
-}, false);
+function showDetailFour() {
+    document.getElementById('four').firstChild.style.opacity = '0%';
+    document.getElementById('four').lastChild.style.opacity = '100%';
+}
+let slideElementFour = document.getElementById('four');
+slideElementFour.addEventListener('pointerdown', showDetailFour, false);
+function hideDetailFour() {
+    document.getElementById('four').firstChild.style.opacity = '100%';
+    document.getElementById('four').lastChild.style.opacity = '0%';
+}
+slideElementFour.addEventListener('pointerup', hideDetailFour, false);
+
+function showDetailFive() {
+    document.getElementById('five').firstChild.style.opacity = '0%';
+    document.getElementById('five').lastChild.style.opacity = '100%';
+}
+let slideElementFive = document.getElementById('five');
+slideElementFive.addEventListener('pointerdown', showDetailFive, false);
+function hideDetailFive() {
+    document.getElementById('five').firstChild.style.opacity = '100%';
+    document.getElementById('five').lastChild.style.opacity = '0%';
+}
+slideElementFive.addEventListener('pointerup', hideDetailFive, false);
+
+function showDetailSix() {
+    document.getElementById('six').firstChild.style.opacity = '0%';
+    document.getElementById('six').lastChild.style.opacity = '100%';
+}
+let slideElementSix = document.getElementById('six');
+slideElementSix.addEventListener('pointerdown', showDetailSix, false);
+function hideDetailSix() {
+    document.getElementById('six').firstChild.style.opacity = '100%';
+    document.getElementById('six').lastChild.style.opacity = '0%';
+}
+slideElementSix.addEventListener('pointerup', hideDetailSix, false);
